81c846b7c383ba3e9f3b4abcc5d5b2cc775191b1
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-05-11 18:52:46 +0200

    solution for 18 and 68

--- 18.hs
+++ 18.hs
@@ -1,13 +1,31 @@
 module Euler
        where
+import Data.List
+import qualified Data.ByteString.Lazy.Char8 as L
 
-data Tree a= Leaf a
-          | Branch (Tree a) a (Tree a)
-            deriving (Show)
-            
-convertTabsInTree :: [Int] -> [Int] -> [Tree Int]
-convertTabsInTree top bottom
-  | null bottom = map Leaf top
-            
+readDaFile fileName= do
+  inpStr <- L.readFile fileName
+  let listNumbers = processFile inpStr
+  putStrLn $ show listNumbers
+  return (processDaBordel (reverse listNumbers))
+ 
+processFile :: L.ByteString -> [[Int]]
+processFile = map (map read) . map (map L.unpack) . map (L.split ' ') . L.lines
 
-test = convertTabsInTree [7, 6] []
+processDaBordel :: [[Int]] -> [Int]
+processDaBordel (x:y:xs)  = processDaBordel newNums
+  where newNums = newblast : xs
+        path = getPath x
+        newblast = zipWith (+) y path
+processDaBordel (x:[]) = x
+
+getPath :: [Int] -> [Int]
+getPath bottom
+  | length bottom == 1 = bottom
+  | otherwise = map getPairMax [0 .. (length bottom -2)]
+    where
+        getPairMax i = max ((!!) bottom i) ((!!) bottom (i+1))
+testMerge = getPath [4,5,6]
+
+go = readDaFile "triangle.txt" 
+
