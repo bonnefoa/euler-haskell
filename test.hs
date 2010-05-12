4c2c0fc95b918bd09789d6414f277cc2e54c7979
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-05-12 08:06:33 +0200

    Some quickCheck tests

--- test.hs
+++ test.hs
@@ -1,17 +1,22 @@
-module Main
-       where
+import Data.List
+import Test.QuickCheck
 
-x=5
-y = (6,"Hello")
-z = x * fst y
-
-main = putStrLn "Hello World"
-          
-square x =x *x
-
-f x = 
-  case x of
-    0 ->1
-    1 -> 5
-    2 -> 2
-    _ -> -1
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+  where lhs = filter (< x) xs
+        rhs = filter (>= x) xs
+        
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+prop_minimun xs = not (null xs) ==>  head (qsort xs) == minimum xs
+prop_ordered xs = ordered (qsort xs)
+  where ordered [] = True
+        ordered [x] = True
+        ordered (x:y:xs) = x <= y && ordered (y:xs)
+prop_permutation xs = permutation xs (qsort xs)
+  where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+prop_maximum xs = not (null xs) ==> last (qsort xs) == maximum xs        
+prop_append xs ys = not (null xs) ==> 
+                    not (null ys) ==>
+                    head (qsort(xs++ys)) == min (minimum xs) (minimum ys)
+                    
